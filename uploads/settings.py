3285a96c04fd6063070577fd3c2815afe843fc87
Krzysztof <50321802+1998krzysiek@users.noreply.github.com>
2021-05-04 19:43:27 +0200

    creating domain

--- uploads/settings.py
+++ uploads/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e#-^aknk(5k)ej6rh#h$i(%h(m9)-j*lwrc_1dxnk=a@-mixlt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'chatbotpolityczny.herokuapp.com']
 
 
 # Application definition
